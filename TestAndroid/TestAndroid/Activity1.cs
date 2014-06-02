a8995915adfb99562d89681bad4ee637b9d3c9fe
vadzimrudkouski@gmail.com <vadzimrudkouski@gmail.com>
2014-06-02 17:40:59 +0300

    c

--- TestAndroid/TestAndroid/Activity1.cs
+++ TestAndroid/TestAndroid/Activity1.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Mindscape.Raygun4Net;
 
 namespace TestAndroid
 {
@@ -14,21 +15,22 @@ namespace TestAndroid
     {
         int count = 1;
 
-        //protected override void OnCreate(Bundle bundle)
-        //{
-        //    base.OnCreate(bundle);
+        protected override void OnCreate(Bundle bundle)
+        {
+            RaygunClient.Attach("JNZwtZ68Y1BhfA0JWtJBUw==");
+            base.OnCreate(bundle);
 
-        //    Toast.MakeText(this, "Activity on create", ToastLength.Long).Show();
+            Toast.MakeText(this, "Activity on create", ToastLength.Long).Show();
 
-        //    // Set our view from the "main" layout resource
-        //    //SetContentView(Resource.Layout.Main);
+            // Set our view from the "main" layout resource
+            //SetContentView(Resource.Layout.Main);
 
-        //    // Get our button from the layout resource,
-        //    // and attach an event to it
-        //    //Button button = FindViewById<Button>(Resource.Id.MyButton);
+            // Get our button from the layout resource,
+            // and attach an event to it
+            //Button button = FindViewById<Button>(Resource.Id.MyButton);
 
-        //    //button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
-        //}
+            //button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+        }
     }
 }
 
